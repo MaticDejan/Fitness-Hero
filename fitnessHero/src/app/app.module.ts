b4a1476e98cfa8b36608044c45794972576bfa7a
Ness-eduard <P3720122@ness.com>
2023-03-27 21:49:15 +0300

    Created card component and implemented HeroModel interface

--- fitnessHero/src/app/app.module.ts
+++ fitnessHero/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
