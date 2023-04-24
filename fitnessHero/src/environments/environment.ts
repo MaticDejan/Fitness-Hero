008be2aae1033347c7b74013ac73b1d2e48e7f78
MaticDejan <matic.dejan.andrei@gmail.com>
2023-04-24 20:12:19 +0300

    update firebase database

--- fitnessHero/src/environments/environment.ts
+++ fitnessHero/src/environments/environment.ts
@@ -3,12 +3,11 @@
 export const environment = {
   production:false,
   firebase: {
-    apiKey: "AIzaSyB-4b9OYdX973pCz7Dd4FrvrBRKvj4T3QU",
-    authDomain: "fitness-hero-aaf9c.firebaseapp.com",
-    databaseURL: "https://fitness-hero-aaf9c-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fitness-hero-aaf9c",
-    storageBucket: "fitness-hero-aaf9c.appspot.com",
-    messagingSenderId: "354138753699",
-    appId: "1:354138753699:web:79cc1b2ff41c24f5264d16"
+    apiKey: "AIzaSyDaeO9epmOP-BXkusYjTk-kNk1j1NghvEo",
+    authDomain: "parking-spots-76cb6.firebaseapp.com",
+    projectId: "parking-spots-76cb6",
+    storageBucket: "parking-spots-76cb6.appspot.com",
+    messagingSenderId: "573376327404",
+    appId: "1:573376327404:web:bb84b6e84b0afeed66ffc9"
   }
 };
